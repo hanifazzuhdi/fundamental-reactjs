b50898bf5951915bad03c77a0860fac5257560cb
hanifazzuhdi <hnfhanif52@gmail.com>
2021-04-05 10:02:36 +0700

    consume api post, put

--- src/component/blog/BlogCard.js
+++ src/component/blog/BlogCard.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
 const BlogCard = (props) => {
-    return (
+    return (  
         <div className="card-post">
             <img className="thumb-post" src={props.thumb} alt="thumb post"/>
             <div className="content-post">
                 <p className="title-post">{props.data.title}</p>
                 <p className="body-post">{props.data.body}</p>
                 <button className="btn-remove" onClick={() => props.remove(props.data.id)}>Remove</button>
+                <button className="btn-update" onClick={() => props.update(props.data)}>Update</button>
             </div>
         </div>
     )
 }
 
 BlogCard.defaultProps = {
-    thumb: "https://placeimg.com/200/150/tech",
+    thumb: "https://source.unsplash.com/200x200/?nature,water,tech",
     title: "Judul Konten",
     body : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, hic ullam libero animi suscipit aut."
 }
--- src/container/blog/Blog.js
+++ src/container/blog/Blog.js
@@ -9,25 +9,20 @@ export default class Blog extends Component{
         super(props);
 
         this.state = {
-            data: []
+            data: [],
+            post: {
+                userId: 1,
+                id: 1,
+                title: '',
+                body: ''
+            },
+            isUpdate: false
         }
     }
-
-    // Note : props bisa diisi dengan fungsi
-    handleRemove = (id) => {
-        // console.log(id);
-
-        axios.delete(`http://localhost:3004/posts/${id}`)
-            .then(response => {
-                console.log(response);
-                
-                this.getPostApi();
-            })
-    }
-
-    // Menggunakan Axios
+ 
+    // API
     getPostApi = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
         .then(response => {
             // console.log(response.data);
             this.setState({
@@ -36,6 +31,92 @@ export default class Blog extends Component{
         })
     }
 
+    postData = () => {
+        axios.post('http://localhost:3004/posts', this.state.post)
+            .then(() => {
+                this.setState({
+                    post: {
+                        userId: 1,
+                        id: 1,
+                        title: '',
+                        body: ''
+                    }
+                });
+
+                this.getPostApi()
+            }, (err) => {
+                alert(`error : ${err}`);
+            });
+    }
+
+    updateData = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.post.id}`, this.state.post)
+            .then(() => {
+                this.setState({
+                    post: {
+                        userId: 1,
+                        id: 1,
+                        title: '',
+                        body: ''
+                    },
+                    isUpdate: false
+                });
+
+                this.getPostApi();
+            });
+    }
+
+
+    // Handle Action   
+    handleRemove = (id) => {
+        axios.delete(`http://localhost:3004/posts/${id}`)
+            .then(response => {
+                console.log(response);
+                
+                this.getPostApi();
+            })
+    }
+
+    handleInput = (event) => {
+        let postNew = {...this.state.post};
+
+        if (!this.state.isUpdate){
+            let id = new Date().getTime();
+            postNew['id'] = id;
+        }
+        postNew[event.target.name] = event.target.value;
+
+        this.setState({
+            post: postNew
+        });
+    }
+
+    handleUpdate = (data) => {
+        console.log(data);
+
+        this.setState({
+            post: data,
+            isUpdate: true
+        })
+
+    }
+
+    handleSubmit = () => {
+        if (this.state.post.title === '' && this.state.post.body === ''){
+            return alert('Semua inputan tidak boleh kosong !');
+        }
+
+        if (!this.state.isUpdate){
+            this.postData();
+        }
+
+        this.updateData();
+        // const input = document.querySelectorAll('.form');
+        // [...input].map(res => res.value = '');
+    }
+
+
+    // Cycle
     componentDidMount(){
         this.getPostApi();
     }
@@ -45,9 +126,24 @@ export default class Blog extends Component{
             <Fragment>
                 <p>React Blog Consume API</p>
                 <hr/>
+                <h3>Add Post</h3>
+                <div className="form-post">
+                    <label className="form-label">Title : </label>
+                    <input className="form-input form" value={this.state.post.title} type="text" name="title" onChange={this.handleInput}/>
+               
+                    <label className="form-label">Content : </label>
+                    <textarea className="form-textarea form" value={this.state.post.body} name="body" rows="10" onChange={this.handleInput}></textarea>
+               
+                    {
+                        this.state.isUpdate === true 
+                                ? <button className="form-button" onClick={this.handleSubmit}>Update</button> 
+                                : <button className="form-button" onClick={this.handleSubmit}>Save</button>
+                    }
+                    
+                </div>
                 <div className="cards">
                 {
-                    this.state.data.map(data => <BlogCard key={data.id} data={data} remove={this.handleRemove}/>)
+                    this.state.data.map(data => <BlogCard key={data.id} data={data} remove={this.handleRemove} update={this.handleUpdate} />)
                 }
                 </div>
             </Fragment>
